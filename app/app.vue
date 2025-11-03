b262f36f9f91cd7f77ee995f64f9658bc5212bde
Fredo-Ronan <fredocode06@gmail.com>
2025-11-03 11:29:05 +0700

    add first commit nuxt js + tailwindcss boilerplate

---
+++ app/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="h-screen flex justify-center items-center bg-blue-950 text-white">
+    <div class="text-center">
+      <h1 class="text-2xl font-bold">Nuxt JS + Tailwind CSS boilerplate</h1>
+      <p class="text-lg">
+        Made by
+        <nuxt-link href="https://fredo-code.vervel.app" target="_blank" class="underline">Fredo Ronan</nuxt-link>
+      </p>
+      <p class="mt-6">
+        Edit file
+        <code class="font-mono bg-black text-white rounded px-2 text-sm">app.vue</code>
+        to get started
+      </p>
+    </div>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  tailwindcss: {
+    cssPath: './app/assets/css/tailwind.css',
+    config: {},
+    viewer: {
+      endpoint: '/_tailwind',
+      exportViewer: true
+    },
+    exposeConfig: false
+  }
+})
