3cb88b92af9d56a39eb0c5354d7b482b45800fbe
Fredo-Ronan <fredocode06@gmail.com>
2025-11-07 00:12:32 +0700

    add eslint and server api example

--- app/app.vue
+++ app/app.vue
@@ -1,16 +1,39 @@
+<script lang="ts" setup>
+	const { data: helloData, error, status } = await useFetch("/api/v1/hello");
+</script>
+
 <template>
-  <div class="h-screen flex justify-center items-center bg-blue-950 text-white">
-    <div class="text-center">
-      <h1 class="text-2xl font-bold">Nuxt JS + Tailwind CSS boilerplate</h1>
-      <p class="text-lg">
-        Made by
-        <nuxt-link href="https://fredo-code.vercel.app" target="_blank" class="underline">Fredo Ronan</nuxt-link>
-      </p>
-      <p class="mt-6">
-        Edit file
-        <code class="font-mono bg-black text-white rounded px-2 text-sm">app.vue</code>
-        to get started
-      </p>
-    </div>
-  </div>
+	<div class="h-screen flex justify-center items-center bg-blue-950 text-white">
+		<div class="text-center">
+			<h1 class="text-2xl font-bold">
+				Nuxt JS + Tailwind CSS boilerplate
+			</h1>
+			<p class="text-lg">
+				Made by
+				<nuxt-link
+					href="https://fredo-code.vercel.app"
+					target="_blank"
+					class="underline"
+				>Fredo Ronan</nuxt-link>
+			</p>
+			<p class="mt-6">
+				Edit file
+				<code class="font-mono bg-black text-white rounded px-2 text-sm">app.vue</code>
+				to get started
+			</p>
+			<p class="mt-6">
+				Visit <code class="font-mono bg-black text-white rounded px-2 text-sm"><nuxt-link
+					target="_blank"
+					href="http://localhost:3000/_tailwind"
+					class="underline"
+				>http://localhost:3000/_tailwind</nuxt-link></code> to see tailwind classes
+			</p>
+			<p class="mt-6">
+				Data from API: {{ helloData!.hello }}
+			</p>
+			<p>
+				This data is comming from <code class="font-mono bg-black text-white rounded px-2 text-sm">/api/v1/hello</code> route. Check the server folder.
+			</p>
+		</div>
+	</div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ],
-  tailwindcss: {
-    cssPath: './app/assets/css/tailwind.css',
-    config: {},
-    viewer: {
-      endpoint: '/_tailwind',
-      exportViewer: true
-    },
-    exposeConfig: false
-  }
-})
+	modules: [
+		'@nuxtjs/tailwindcss',
+		"@nuxt/eslint",
+	],
+	devtools: { enabled: true },
+	compatibilityDate: "2025-07-15",
+	eslint: {
+		config: {
+			stylistic: {
+				semi: true,
+				quotes: "double",
+				commaDangle: "always-multiline",
+				indent: "tab",
+			},
+		},
+	},
+	tailwindcss: {
+		cssPath: "./app/assets/css/tailwind.css",
+		config: {},
+		viewer: {
+			endpoint: "/_tailwind",
+			exportViewer: true,
+		},
+		exposeConfig: false,
+	},
+});
